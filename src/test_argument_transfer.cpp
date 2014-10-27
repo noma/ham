d61ffa66ebf834b7b8fbac09ae537f4a3d0bc8e3
noma <ma.noack.pr@gmail.com>
2014-10-27 17:47:39 +0100

    Added a new test for argument and return value transfers in HAM-Offload

---
+++ src/test_argument_transfer.cpp
@@ -0,0 +1,93 @@
+// Copyright (c) 2013-2014 Matthias Noack (ma.noack.pr@gmail.com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include "ham/offload.hpp"
+#include <iostream>
+
+using namespace ham;
+
+// offloaded function
+template<typename T>
+T type_transfer_function(T arg)
+{
+	return arg;
+}
+
+// performs the offload for one type and value
+template<typename T>
+bool test_type_invokation(offload::node_t target, T arg)
+{
+	T result = offload::sync(target, f2f(&type_transfer_function<T>, arg));
+	bool passed = result == arg;
+	std::cout << "Result for type \"" << typeid(T).name() << "\": " << (passed ? "pass" : "fail") << std::endl;
+	return passed;
+}
+
+// recursive conjunction over the test results
+template<typename T>
+bool and_reducer(T arg)
+{
+	return arg && true;
+}
+template<typename T, typename... Ts>
+bool and_reducer(T v, Ts... vs)
+{
+	return v && and_reducer(vs...);
+}
+
+
+// performs the test for a list of types and values
+template<typename... Ts>
+bool test_type_transfer(offload::node_t target, Ts... args)
+{
+	return and_reducer(test_type_invokation(target, args)...);
+}
+
+/**
+ * Tests the argument and return value transfer for offloaded function.
+ * For each type, a value is passed as argument and then returned. The test is
+ * passed, if the the passed argument and the return value are equal.
+ */
+int main(int argc, char* argv[])
+{
+	std::cout << "Testing argument and return value transfer for different types:" << std::endl;
+
+	offload::node_t target = 1;
+	bool passed = false;
+	
+	// list of types and values to test, static_cast syntax is not only for types with unclear literals, but also for readability
+	passed = test_type_transfer(target, 
+		// boolean
+		static_cast<bool>(false),
+		// character types
+		static_cast<char>('c'),
+		static_cast<char16_t>(u'c'),
+		static_cast<char32_t>(U'c'),
+		static_cast<wchar_t>(L'c'),
+		// integer types
+		static_cast<signed char>(42),
+		static_cast<short>(1337),
+		static_cast<int>(1337),
+		static_cast<long>(1337l),
+		static_cast<long long>(1337ll),
+		// unsigned integer types
+		static_cast<unsigned char>(42),
+		static_cast<unsigned short>(1337),
+		static_cast<unsigned>(1337),
+		static_cast<unsigned long>(1337ul),
+		static_cast<unsigned long long>(1337ull),
+		// floating point types
+		static_cast<float>(13.37f),
+		static_cast<double>(13.37),
+		static_cast<long double>(13.37l),
+		// null pointer
+		nullptr
+	);
+	
+	std::cout << "Overall result: " << (passed ? "pass" : "fail") << std::endl;
+	
+	return 0;	
+}
+

596e13d2cd33d018be3024438a8586391009495c
noma <ma.noack.pr@gmail.com>
2018-04-03 13:47:34 +0200

    Added define check for HAM_EXPLICIT to avoid compiler warning when set in build-env.

--- src/ham_offload_explicit.cpp
+++ src/ham_offload_explicit.cpp
@@ -3,7 +3,9 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#define HAM_EXPLICIT // set here, before including HAM-Offload, or in your build system
+#if not defined(HAM_EXPLICIT)
+	#define HAM_EXPLICIT // set here, before including HAM-Offload, or in your build system
+#endif
 #include "ham/offload.hpp"
 #include <iostream>
 #include <list>

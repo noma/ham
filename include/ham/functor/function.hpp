d6fa163f8f04ccec14a7667f98d242560c0d0afa
noma <ma.noack.pr@gmail.com>
2018-04-04 19:02:06 +0200

    Fixed f2f to be standard compliant for zero arguments beyond the function ptr.

--- include/ham/functor/function.hpp
+++ include/ham/functor/function.hpp
@@ -9,6 +9,8 @@
 #include <tuple>
 #include <type_traits>
 
+#include <boost/preprocessor/facilities/overload.hpp>
+
 #include "ham/misc/traits.hpp"
 #include "ham/util/make_seq.hpp"
 #include "ham/misc/migratable.hpp"
@@ -75,8 +77,48 @@ private:
  * Sadly, this macro is necessary to achieve the f2f syntax AND store
  * the function pointer as template value parameter.
  */
-#define f2f(function_ptr, ...) ham::function<decltype(function_ptr), function_ptr>(__VA_ARGS__)
+// NOTE: violates the standard, as '...' must get at least one argument, hence the BOOST_PP_OVERLOAD work-around
+//#define f2f(function_ptr, ...) ham::function<decltype(function_ptr), function_ptr>(__VA_ARGS__)
 
+// Macro for the function template instantiation without argument list
+#define F2F_TEMPLATE(function_ptr)    ham::function<decltype(function_ptr), function_ptr>
+// Macro that adds an empty argument list to F2F_TEMPLATE
+#define F2F_ZERO_ARGS(function_ptr)   F2F_TEMPLATE(function_ptr)()
+// Macro that adds a non-empty argument list to F2F_TEMPLATE using variadic macros again
+#define F2F_N_ARGS(function_ptr, ...) F2F_TEMPLATE(function_ptr)(__VA_ARGS__)
+
+// Macros to be used with BOOST_PP_OVERLOAD for F2F_<N> arguments (including function ptr)
+#define F2F_1(function_ptr) F2F_ZERO_ARGS(function_ptr)
+#define F2F_2(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_3(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_4(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_5(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_6(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_7(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_8(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_9(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_10(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_11(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_12(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_13(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_14(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_15(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_16(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_17(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_18(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_19(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_20(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_21(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_22(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_23(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_24(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_25(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_26(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_27(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_28(...)          F2F_N_ARGS(__VA_ARGS__)
+#define F2F_29(...)          F2F_N_ARGS(__VA_ARGS__)
+
+#define f2f(...) BOOST_PP_OVERLOAD(F2F_,__VA_ARGS__)(__VA_ARGS__)
 
 /**
  * Copyability of Function<...> is defined by the conjunction of the copyability of
@@ -90,4 +132,3 @@ struct is_bitwise_copyable<function<Result (*)(Pars...), FunctionPtr>> {
 } // namespace ham
 
 #endif // ham_functor_function_hpp
-

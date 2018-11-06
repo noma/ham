9b9d5d4ed669a0062351e03a2bb24b8ab03e4647
Daniel Deppisch <deppisch@zib.de>
2018-11-06 22:55:51 +0100

    workaround to prevent tcp target from crashing due to connection closing before terminate functor is transmitted and executed

--- include/ham/offload/offload.hpp
+++ include/ham/offload/offload.hpp
@@ -197,6 +197,9 @@ void ping(node_t node, Functor&& func)
 	HAM_DEBUG( HAM_LOG << "runtime::ping(): sending msg..." << std::endl; )
 	net::communicator::request req = comm.allocate_request(node); // TODO(improvement): resource deallocation of this request (currently only used for terminating)
 	comm.send_msg(req, (void*)&msg, sizeof msg);
+#if defined(HAM_COMM_TCP)
+    while(!req.sent())		// ugly workaround to prevent target from crashing because of connection teardown before the terminate functor is executed
+#fi
 	HAM_DEBUG( HAM_LOG << "runtime::ping(): sending msg done." << std::endl; )
 }
 

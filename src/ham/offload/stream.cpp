f6c9f73cb4e3b586a7a97255959a284548cc9a3e
Daniel Deppisch <deppisch@zib.de>
2018-11-09 18:48:52 +0100

    added streams

---
+++ src/ham/offload/stream.cpp
@@ -0,0 +1,6 @@
+// Copyright (c) 2013-2014 Matthias Noack (ma.noack.pr@gmail.com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include "ham/offload/stream.hpp"

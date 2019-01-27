7cd51249216e1e780d00baad3a969da782a49057
noma <ma.noack.pr@gmail.com>
2019-01-27 04:41:40 +0100

    Fixed options, to have the split method as a free function, and disabled command line parsing as a workaround to avoid an issues with CL11 and multiple app objects.

--- include/ham/misc/options.hpp
+++ include/ham/misc/options.hpp
@@ -14,57 +14,45 @@
 namespace ham {
 namespace detail {
 
+std::vector<std::string>& split(const std::string &s, char delim, std::vector<std::string> &elems);
+std::vector<std::string> split(const std::string &s, char delim);
+
 class options {
 public:
 	options(int argc, char* argv[])
 	 : cpu_affinity_(-1)
 	{
-		// command line options
-		CLI::App app("HAM Options");
-		app.add_option("--ham-cpu-affinity", cpu_affinity_, "Per process value for the CPU affinity.");
+// TODO: figure out CLI11 issue, 2018-01-22, and reenable
+// NOTE: multiple App defs seem to be problematic, so disabled this one temporarily for the comm backend to do its thing
+//		// command line options
+//		CLI::App app("HAM Options");
+//		app.add_option("--ham-cpu-affinity", cpu_affinity_, "Per process value for the CPU affinity.");
 
-		// NOTE: no try-catch here to avoid exceptions, that cause offload-dependencies to boost exception in the MIC code
-		const char* options_env = std::getenv("HAM_OPTIONS");
-		if(options_env)
-		{
-			char split_character = ' ';
-			if (std::getenv("HAM_OPTIONS_NO_SPACES")) // value does not matter
-				split_character = '_';
-			// parse from environment
-			// NOTE: get a vector strings from the environment options
-			auto args = split(std::string(options_env), split_character);
-			app.parse(args);
-		}
-		else
-		{
-			// parse from command line
-			try {
-				app.parse(argc, argv);
-			} catch(const CLI::ParseError &e) {
-				app.exit(e);
-			}
-			//app.parse(argc, argv);
-		}
+//		// NOTE: no try-catch here to avoid exceptions, that cause offload-dependencies to boost exception in the MIC code
+//		const char* options_env = std::getenv("HAM_OPTIONS");
+//		if(options_env)
+//		{
+//			char split_character = ' ';
+//			if (std::getenv("HAM_OPTIONS_NO_SPACES")) // value does not matter
+//				split_character = '_';
+//			// parse from environment
+//			// NOTE: get a vector strings from the environment options
+//			auto args = split(std::string(options_env), split_character);
+//			app.parse(args);
+//		}
+//		else
+//		{
+//			// parse from command line
+//			try {
+//				app.parse(argc, argv);
+//			} catch(const CLI::ParseError &e) {
+//				app.exit(e);
+//			}
+//			//app.parse(argc, argv);
+//		}
 	}
 
 	int cpu_affinity() const { return cpu_affinity_; }
-
-	static std::vector<std::string>& split(const std::string &s, char delim, std::vector<std::string> &elems)
-	{
-		std::stringstream ss(s);
-		std::string item;
-		while (std::getline(ss, item, delim)) {
-			elems.push_back(item);
-		}
-		return elems;
-	}
-
-	static std::vector<std::string> split(const std::string &s, char delim)
-	{
-		std::vector<std::string> elems;
-		split(s, delim, elems);
-		return elems;
-	}
 
 private:
 	int cpu_affinity_;

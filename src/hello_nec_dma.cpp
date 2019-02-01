fd5f05046b8b724597dd1582e7faddb34bfc41c0
noma <ma.noack.pr@gmail.com>
2019-02-01 15:09:31 +0100

    Adapted hello_nec_dma to new VH/VE defines.

--- src/hello_nec_dma.cpp
+++ src/hello_nec_dma.cpp
@@ -11,10 +11,10 @@
 using namespace ham;
 
 // compile on VE side only
-#if (HAM_COMM_VEO == 0)
+#if defined(HAM_COMM_VH)
 	#include <sys/ipc.h>
 	#include <sys/shm.h>
-#elif (HAM_COMM_VEO == 1)
+#elif defined(HAM_COMM_VE)
 extern "C" {
 	#include <vhshm.h>
 }
@@ -22,9 +22,15 @@ extern "C" {
 
 uint64_t hello(key_t shm_key, size_t size) 
 {
-std::cout << "hello: HAM_COMM_VEO = "  << HAM_COMM_VEO << std::endl;
+#ifdef HAM_COMM_VH
+std::cout << "hello: HAM_COMM_VH" << std::endl;
+#endif
+#ifdef HAM_COMM_VE
+std::cout << "hello: HAM_COMM_VE" << std::endl;
+#endif
+
 // compile on VE side only
-#if (HAM_COMM_VEO == 1)	
+#ifdef HAM_COMM_VE
 	// https://veos-sxarr-nec.github.io/libsysve/group__vhshm.html#gad8fc6108e95dab9c18c4e56f610f3f03
 	// https://linux.die.net/man/2/shmat
 	// vh_shmget (key_t key, size_t size, int shmflag)
@@ -77,7 +83,12 @@ int main(int argc, char* argv[])
 	HAM_UNUSED_VAR(argc);
 	HAM_UNUSED_VAR(argv);
 
-std::cout << "main: HAM_COMM_VEO = "  << HAM_COMM_VEO << std::endl;
+#ifdef HAM_COMM_VH
+std::cout << "hello: HAM_COMM_VH" << std::endl;
+#endif
+#ifdef HAM_COMM_VE
+std::cout << "hello: HAM_COMM_VE" << std::endl;
+#endif
 
 // NOTE: all f2f's must be visible in the VE to trigger template code generation for active message handlers
 	offload::node_t target = 1; // we simply use the first device/node
@@ -93,14 +104,14 @@ std::cout << "main: HAM_COMM_VEO = "  << HAM_COMM_VEO << std::endl;
 
 	int shm_key = -1;
 // compile on VH only
-#if (HAM_COMM_VEO == 0)	
+#if defined(HAM_COMM_VH)
 	shm_key = shmget(IPC_PRIVATE, size, IPC_CREAT | IPC_EXCL | SHM_HUGETLB); 
 	assert(shm_key != -1);
 #endif
 
 	// attach shared memory to VH address space
 	void* local_addr = nullptr;
-#if (HAM_COMM_VEO == 0)	
+#if defined(HAM_COMM_VE)
 	local_addr = shmat(shm_key, NULL, 0);
 	assert(local_addr != nullptr);
 #endif
@@ -117,7 +128,7 @@ std::cout << "main: HAM_COMM_VEO = "  << HAM_COMM_VEO << std::endl;
 	// TODO: use shm DMA 
 
 	// detach
-#if (HAM_COMM_VEO == 0)	
+#if defined(HAM_COMM_VH)
 	int err = shmdt(local_addr);
 	assert(err == 0);
 #endif
